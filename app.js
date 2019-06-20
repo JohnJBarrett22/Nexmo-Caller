4f2c14ed1949b9379019dc1950b934698bfe261a
John Barrett <johnjbarrett22@gmail.com>
2019-06-19 20:35:12 -0700

    Corrected issues to create a function app using Nexmo API, though cut out needed keys, etc.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const Nexmo = require('nexmo');
 const privateKey = require('fs').readFileSync("private.key");
 
 const nexmo = new Nexmo({
-  apiKey: '',
-  apiSecret: '',
-  applicationId: '',
+  apiKey: settings.apiKey,
+  apiSecret: settings.apiSecret,
+  applicationId: settings.applicationId,
   privateKey: privateKey,
 });
 
@@ -13,17 +13,19 @@ const ncco = [
       action: 'talk',
       voiceName: 'Salli',
       text:
-        'What do you get if you cross a telephone with an iron? A smooth operator!',
+        'Test test test test test test test test!',
     },
   ];
 
   nexmo.calls.create(
     {
-      to: [{ type: 'phone', number: '' }],
-      from: { type: 'phone', number: '' },
+      to: [{ type: 'phone', number: '15555555555' }],
+      from: { type: 'phone', number: '15555555555' },
       ncco,
     },
     (err, result) => {
       console.log(err || result);
     },
   );
+
+  
